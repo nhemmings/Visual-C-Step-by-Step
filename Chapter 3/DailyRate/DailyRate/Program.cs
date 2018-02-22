e6faf12dd0442a0c37868005438592973dea8877
Nathan Hemmings <natehemm@gmail.com>
2018-02-21 20:18:38 -0600

    Chapter 3 - DailyRate

--- Chapter 3/DailyRate/DailyRate/Program.cs
+++ Chapter 3/DailyRate/DailyRate/Program.cs
@@ -15,7 +15,27 @@ namespace DailyRate
 
         void run()
         {
+            double dailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter the number of days: ");
+            writeFee(calculateFee(dailyRate, noOfDays));
+        }
 
+        private void writeFee(double v) => Console.WriteLine($"The consultant's fee is: {v * 1.1}");
+
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+        private int readInt(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+            return int.Parse(line);
+        }
+
+        private double readDouble(string v)
+        {
+            Console.Write(v);
+            string line = Console.ReadLine();
+            return double.Parse(line);
         }
     }
 }
